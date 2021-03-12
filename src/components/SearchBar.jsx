e735cfa3ce838aa3b7d66dfa5f345610248e7cb2
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-12 09:43:12 -0300

    feat(searchbar.jsx): import css module

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import styles from '../styles/components/SearchBar.module.css'
+
 export const SearchBar = () => {
   return (
-    <form>
+    <form className={styles.searchBarContainer}>
       <input type='search' />
       <input type='submit' value='Pesquisar' />
     </form>
