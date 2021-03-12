ce170fdfe2d3575bb70776b7acba6713b1230d70
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-12 10:40:47 -0300

    feat: create user component

---
+++ src/components/User.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export const User = () => {
+  return (
+    <div>
+      <img
+        src='https://avatars.githubusercontent.com/u/43142456?v=4'
+        alt=''
+        width='200px'
+      />
+      <ul>
+        <h2>Dados do Usuario:</h2>
+        <li>Nome: Erick Aguilar</li>
+        <li>Username: ericksvaguilar</li>
+        <li>Seguidores: 10</li>
+        <li>Repositorios: 20</li>
+      </ul>
+      <ul>
+        <h2>Novos Repositorios:</h2>
+        <li>react-notes</li>
+        <li>stopwatch</li>
+        <li>erickaguilar.com.br</li>
+        <li>calculator</li>
+      </ul>
+    </div>
+  )
+}
