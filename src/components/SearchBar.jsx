d4260aaf49906393f837125aab1ec91a1c46ddd7
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-12 09:37:10 -0300

    feat(searchbar.jsx): create search bar component

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const SearchBar = () => {
+  return (
+    <form>
+      <input type='search' />
+      <input type='submit' value='Pesquisar' />
+    </form>
+  )
+}
