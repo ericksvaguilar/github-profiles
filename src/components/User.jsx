f3b42ed8859b89b7babc9d9777619681542a679b
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-12 11:16:35 -0300

    feat: remove img from userdata div

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -5,15 +5,14 @@ import styles from '../styles/components/User.module.css'
 export const User = props => {
   return (
     <div className={styles.userContainer}>
+      <img
+        src={
+          props.profileUrl ||
+          'https://avatars.githubusercontent.com/u/43142456?v=4'
+        }
+        alt={props.name}
+      />
       <div className={styles.userData}>
-        <img
-          src={
-            props.profileUrl ||
-            'https://avatars.githubusercontent.com/u/43142456?v=4'
-          }
-          alt={props.name}
-        />
-
         <ul>
           <h2>Dados do Usuário</h2>
           <li>{`Nome: ${props.name}`}</li>
