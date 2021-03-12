8f27182de35391a35ffdf30ca6b7773ecd3a522e
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-12 11:14:20 -0300

    feat: create user data class

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -5,21 +5,23 @@ import styles from '../styles/components/User.module.css'
 export const User = props => {
   return (
     <div className={styles.userContainer}>
-      <img
-        src={
-          props.profileUrl ||
-          'https://avatars.githubusercontent.com/u/43142456?v=4'
-        }
-        alt={props.name}
-      />
+      <div className={styles.userData}>
+        <img
+          src={
+            props.profileUrl ||
+            'https://avatars.githubusercontent.com/u/43142456?v=4'
+          }
+          alt={props.name}
+        />
 
-      <ul>
-        <h2>Dados do Usuário</h2>
-        <li>{`Nome: ${props.name}`}</li>
-        <li>{`Username: ${props.username}`}</li>
-        <li>{`Seguidores: ${props.followersCount}`}</li>
-        <li>{`Repositórios: ${props.repositoriesCount}`}</li>
-      </ul>
+        <ul>
+          <h2>Dados do Usuário</h2>
+          <li>{`Nome: ${props.name}`}</li>
+          <li>{`Username: ${props.username}`}</li>
+          <li>{`Seguidores: ${props.followersCount}`}</li>
+          <li>{`Repositórios: ${props.repositoriesCount}`}</li>
+        </ul>
+      </div>
     </div>
   )
 }
