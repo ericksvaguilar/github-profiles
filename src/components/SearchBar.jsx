40efe2f684d64c6f8454718fa4d5f1b5d8bfcc66
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-16 06:21:20 -0300

    feat: add onchange event to search input field

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 import styles from '../styles/components/SearchBar.module.css'
 
-export const SearchBar = () => {
+export const SearchBar = props => {
   return (
     <form className={styles.searchBarContainer}>
-      <input type='search' />
+      <input type='search' onChange={props.handleChange} />
       <input type='submit' value='Pesquisar' />
     </form>
   )
