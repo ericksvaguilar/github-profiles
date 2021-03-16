b327e5c17fb1a5ccb052fc22913c32afb43e1efc
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-16 06:27:34 -0300

    feat: add handlechange feature on app component

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ import { User } from './components/User'
 import styles from './styles/App.module.css'
 
 export const App = () => {
+  const handleChange = e => {
+    console.log(e.target.value)
+  }
+
   return (
     <div className={styles.appContainer}>
-      <SearchBar />
+      <SearchBar onChange={e => handleChange(e)} />
       <User />
     </div>
   )
