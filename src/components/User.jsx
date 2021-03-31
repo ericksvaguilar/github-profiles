ae78bfb5150200800cf285834656a90aec0734ff
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-31 11:47:06 -0300

    feat: insert conditional render

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -4,26 +4,24 @@ import PropTypes from 'prop-types'
 import styles from '../styles/components/User.module.css'
 
 export const User = props => {
-  return (
-    <div className={styles.userContainer}>
-      <img
-        src={
-          props.profileUrl ||
-          'https://avatars.githubusercontent.com/u/43142456?v=4'
-        }
-        alt={props.name}
-      />
-      <div className={styles.userData}>
-        <ul>
-          <h2>Dados do Usuário</h2>
-          <li>{`Nome: ${props.name}`}</li>
-          <li>{`Username: ${props.username}`}</li>
-          <li>{`Seguidores: ${props.followersCount}`}</li>
-          <li>{`Repositórios: ${props.repositoriesCount}`}</li>
-        </ul>
+  if (props.profileUrl) {
+    return (
+      <div className={styles.userContainer}>
+        <img src={props.profileUrl} alt={props.name} />
+        <div className={styles.userData}>
+          <ul>
+            <h2>Dados do Usuário</h2>
+            <li>{`Nome: ${props.name}`}</li>
+            <li>{`Username: ${props.username}`}</li>
+            <li>{`Seguidores: ${props.followersCount}`}</li>
+            <li>{`Repositórios: ${props.repositoriesCount}`}</li>
+          </ul>
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return <></>
+  }
 }
 
 User.propTypes = {
