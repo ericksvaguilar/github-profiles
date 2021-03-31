cc6c1428b437c771352d1a3e77f7444dd996decd
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-31 11:31:52 -0300

    fix: correct submit props name

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ import styles from '../styles/components/SearchBar.module.css'
 
 export const SearchBar = props => {
   return (
-    <form className={styles.searchBarContainer}>
+    <form className={styles.searchBarContainer} onSubmit={props.onSubmit}>
       <input type='search' onChange={props.onChange} />
       <input type='submit' value='Pesquisar' />
     </form>
