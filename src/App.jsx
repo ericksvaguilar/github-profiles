8ee71741b398b31c495f4fdeb382ce9728bcda96
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-31 11:31:33 -0300

    feat: add handle submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { SearchBar } from './components/SearchBar'
 import { User } from './components/User'
 
@@ -11,18 +11,22 @@ export const App = () => {
     setUsername(e.target.value)
   }
 
-  useEffect(() => {
+  const handleSubmit = e => {
+    e.preventDefault()
     fetch(`https://api.github.com/users/${username}`)
       .then(response => response.json())
       .then(
         user => console.log(user),
         error => console.error(error)
       )
-  }, [username])
+  }
 
   return (
     <div className={styles.appContainer}>
-      <SearchBar onChange={e => handleChange(e)} />
+      <SearchBar
+        onSubmit={e => handleSubmit(e)}
+        onChange={e => handleChange(e)}
+      />
       <User
         profileUrl={''}
         name={'Erick Aguilar'}
